b7dc305837a6712e9cb760cd779dde9a7a7d770e
abdalftp <45959255+abdalftp@users.noreply.github.com>
2019-01-02 01:13:06 -0800

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'blossom';
+$db['default']['username'] = 'gfincuct_bbp';
+$db['default']['password'] = 'rahul!123singh';
+$db['default']['database'] = 'gfincuct_blossom';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
